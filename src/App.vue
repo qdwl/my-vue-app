f119469ef97d319399e2bf016cfc8e220b959347
qdwl <outshine@126.com>
2023-12-05 13:43:52 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <RouterView></RouterView>
+</template>
+
+<style>
+  #app {
+    text-align: center;
+  }
+</style>
